6572c51415b0ee268db1b6d0e4461162222f05df
Akbar Abustang <akbar@tabsquare.com>
2021-05-18 22:28:48 +0700

    emit example

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 let app = require('express')();
 let port = 3000;
-let htpp = require('http').Server(app);
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname +'/index.html');
 });
 
-htpp.listen(port, () => {
+http.listen(port, () => {
   console.log('Connected');
+});
+
+io.on('connection', (socket) => {
+  console.log('socket connected')
+
+  socket.on('disconnect', () => {
+    console.log('disconnected');
+  });
+
+  socket.on('send-message', (data) => {
+    socket.broadcast.emit('send-message', (data));
+    console.log(data)
+  });
+
+
 });
 
