8bb94d6825a05085e02c804298e13195d248a222
Akbar Abustang <akbar@tabsquare.com>
2021-05-18 21:01:19 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let app = require('express')();
+
+let htpp = require('http').Server(app);
+
+app.get('/', (req, res) => {
+  res.send('its working');
+});
+
+htpp.listen(3000, () => {
+  console.log('Connected');
+});
